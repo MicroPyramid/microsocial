65a4fa4ea8f25deb583f3c4a492ba434c6b1f799
aswanii <aswanii@gmail.com>
2013-10-16 11:25:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+
 setup(
       name='microsocial',
       version='0.0.1',
-      #install_requires=['lxml', 'PIL'],
+      install_requires=['requests==2.0.0', 'requests_oauthlib==0.3.2'],
       description='microsocial is collection of social apis for python. It supports authentication, posting, sharing etc.',
       author='MicroPyramid',
       author_email='MicroPyramid@googlegroups.com',
@@ -19,4 +24,12 @@ setup(
       packages=['microsocial'],
       include_package_data=True,
       zip_safe=True,
-      )
+      classifiers=[
+        'Development Status :: 0 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Communications :: Chat',
+        'Topic :: Internet'
+    ]
+)
